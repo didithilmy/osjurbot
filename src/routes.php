0b77c59be760aa86e2180d5042f8d24344cd8837
jrandiny <jrandiny2@gmail.com>
2018-07-05 15:39:21 +0700

    Initial commit

--- src/routes.php
+++ src/routes.php
@@ -12,3 +12,39 @@ $app->get('/[{name}]', function (Request $request, Response $response, array $ar
     // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
 });
+
+$app->post('/addNama', function (Request $request, Response $response, array $args) {
+
+    // Render index view
+    return $this->renderer->render($response, 'index.phtml', $args);
+});
+
+$app->get('/listBasecamp', function (Request $request, Response $response, array $args) {
+
+    // Render index view
+    return $this->renderer->render($response, 'index.phtml', $args);
+});
+
+$app->post('/sampai', function (Request $request, Response $response, array $args) {
+
+    // Render index view
+    return $this->renderer->render($response, 'index.phtml', $args);
+});
+
+$app->post('/pulang', function (Request $request, Response $response, array $args) {
+
+    // Render index view
+    return $this->renderer->render($response, 'index.phtml', $args);
+});
+
+$app->get('/listCurrent', function (Request $request, Response $response, array $args) {
+
+    // Render index view
+    return $this->renderer->render($response, 'index.phtml', $args);
+});
+
+$app->post('/tambahBlacklist', function (Request $request, Response $response, array $args) {
+
+    // Render index view
+    return $this->renderer->render($response, 'index.phtml', $args);
+});
